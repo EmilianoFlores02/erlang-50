09b525e36b46c92e571e9d9cfd00a9e4948c64ad
Emiliano Rafael Flores Ramírez <L20212404@tectijuana.edu.mx>
2024-10-09 22:16:22 -0700

    Create hola_mundo.erl

---
+++ hola_mundo.erl
@@ -0,0 +1,14 @@
+%------------------------------------------------------------------------------
+% Módulo:       Hola mundo
+%
+% Autor:        Emiliano Rafael Flores Ramírez
+% Fecha:        3 de octubre de 2024
+%
+% Descripción: 
+%------------------------------------------------------------------------------
+
+saludar() ->
+    % `io:format/1` es una función incorporada que se utiliza para la salida de texto.
+    % Toma una cadena de formato y la imprime en la salida estándar.
+    % "~n" es un especificador de formato que representa un carácter de nueva línea.
+    io:format("Hola, Mundo!~n").
